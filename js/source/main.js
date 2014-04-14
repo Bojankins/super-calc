2f0092a119e0c3c197408f82fc4b2ff4116732a3
MattOlson <Rs_Bo_Jankins@yahoo.com>
2014-04-14 18:02:31 -0500

    supercalc

--- js/source/main.js
+++ js/source/main.js
@@ -9,17 +9,75 @@
     $('.decimal').click(dec);
     $('.sign').click(sign);
     $('#title').click(title);
+    $('.operator').click(operator);
+    $('#push').click(push);
   }
 
-  function title() {
-    var display = $('#calculator').css('display');
+  function operator() {
+      var op = $(this).data('op');
+      var x = $('#queue > div:nth-child(1)').text() * 1;
+      var y = $('#queue > div:nth-child(2)').text() * 1;
+      var result;
+      
+        switch(op){
+          case 'add':
+            result = x + y;
+            break;
+          case 'sub':
+            result = x - y;
+            break;
+          case 'mul':
+            result = x * y;
+            break;
+          case 'div':
+            result = x / y;
+            break;
+          case 'root':
+            result = Math.sqrt(x);
+            break;
+          case 'fac':
+            result =factorial(x);
+            break;
+          case 'sum':
+            result = sum();
+            break;
+          case 'exp':
+            result = Math.pow(x, y);
+        }
+          $('#display').text(result);
+      
+     }
 
-    if(display === 'none'){
-      $('#calculator').fadeIn();
-    }else{
-      $('#calculator').fadeOut();
+    function sum(){
+          var total = 0;
+
+          $('#queue > div').each(function(index, div){
+            var x = div.textContent * 1;
+            total += x;
+          });
+
+          return total;
+          }
+
+    function factorial(x) {
+        var product = 1;
+
+        for(var i = 2; i <= x; i++){
+          product *= i;
+        }
+
+        return product;
+      }
+
+    function title() {
+      var display = $('#calculator').css('display');
+
+      if(display === 'none'){
+        $('#calculator').fadeIn();
+      }else{
+        $('#calculator').fadeOut();
+      }
     }
-  }
 
   function number() {
     var num = this.textContent;
@@ -34,11 +92,22 @@
     $('#display').text(output);
   }
 
-  function clear() {
+  function push(){
+    var display = $('#display').text();
+    $('#display').text(0);
+    var $div = $('<div>');
+    $div.text(display);
+    $('#queue').prepend($div);
+  }
+
+  function clear(){
     var content = this.textContent;
+
     if(content === 'C'){
       $('#display').text(0);
-    }
+    }else{
+      $('#queue').empty();
+    } 
   }
 
   function dec() {
@@ -54,6 +123,7 @@
   function sign() {
     var display = $('#display').text();
     $('#display').text(display * -1);
-  }
+}
 
+  
 })();
